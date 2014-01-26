3e73ece1d4cb050d9b9140afcf4997d1ef1e1f39
Asher Wood <awood5659@gmail.com>
2014-01-26 11:42:07 -0800

    Create a fragment

--- src/zeklandia/android/printerDisplayMessageEditor/MainActivity.java
+++ src/zeklandia/android/printerDisplayMessageEditor/MainActivity.java
@@ -20,11 +20,6 @@ package zeklandia.android.printerDisplayMessageEditor;
  *			       });
  */
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -32,7 +27,6 @@ import java.util.List;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -46,28 +40,13 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListAdapter;
-import android.widget.RadioGroup;
-import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity {
 
 	SectionsPagerAdapter mSectionsPagerAdapter;
 
 	static ViewPager mViewPager;
-	
-	private static final int printerPort = 9100;
-    private final static String formatRDYMSG = "\u001B%-12345X@PJL JOB\n" +
-    "@PJL RDYMSG DISPLAY=\"{0}\"\n@PJL EOJ\n\u001B%-12345X\n";
-    private final static String formatOPMSG = "\u001B%-12345X@PJL JOB\n" +
-    "@PJL OPMSG DISPLAY=\"{0}\"\n@PJL EOJ\n\u001B%-12345X\n";
-    private final static String formatERRMSG = "\u001B%-12345X@PJL JOB\n" +
-    "@PJL ERRMSG DISPLAY=\"{0}\"\n@PJL EOJ\n\u001B%-12345X\n";
-    
-	static EditText editTextIP;
-	static EditText editTextMessage;
 	
 	Intent helpActivity;
 	Intent aboutActivity;
@@ -76,7 +55,6 @@ public class MainActivity extends FragmentActivity {
 	List<HashMap<String, String>> mPrinterListData = new ArrayList<HashMap<String, String>>();
 	static String[] mPrinterListFrom = new String[]{"PRINTER_NAME", "PRINTER_IP"};
 	static int[] mPrinterListTo = new int[]{R.id.title_text, R.id.subtitle_text};
-	static final boolean debug = false;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -176,7 +154,7 @@ public class MainActivity extends FragmentActivity {
 			int resId = (Integer) getArguments().get(ARG_SECTION_NUMBER);
 			switch (resId) {
 			case 0:
-				resId = R.layout.main;
+				resId = R.layout.main_activity;
 				break;
 			case 1:
 				resId = R.layout.helplayout;
@@ -190,125 +168,5 @@ public class MainActivity extends FragmentActivity {
 			return view;
 		}
 	}
-
-private class sendRDYMSGTask extends AsyncTask<String, Void, Integer> {
-        public static final int TOO_LONG_ERROR = 0;
-        public static final int CONNECTION_ERROR = 1;
-        public static final int SUCESS = 2;
-        
-        protected Integer doInBackground(String... params) {
-                return sendRDYMSG(params[0], params[1]);
-        }
-        
-        protected void onPostExecute(Integer result) {
-                switch(result) {
-                case SUCESS:
-                        Toast.makeText(MainActivity.this, "Sent", Toast.LENGTH_SHORT).show();
-                        break;
-                case CONNECTION_ERROR:
-                        Toast.makeText(MainActivity.this, "An error occured", Toast.LENGTH_SHORT).show();
-                        break;
-                default:
-                        break;
-                }
-        }
-        
-        int sendRDYMSG(String ip, String message) {
-                try {
-                        Socket socket = new Socket(ip, printerPort);
-                        OutputStream out = socket.getOutputStream();
-                        out.write(MessageFormat.format(formatRDYMSG, message).getBytes());
-                        out.close();
-                        socket.close();
-                } catch (UnknownHostException e) {
-                        e.printStackTrace();
-                        return CONNECTION_ERROR;
-                } catch (IOException e) {
-                        e.printStackTrace();
-                        return CONNECTION_ERROR;
-                }
-                return SUCESS;
-        }
-}
-
-private class sendERRMSGTask extends AsyncTask<String, Void, Integer> {
-    public static final int TOO_LONG_ERROR = 0;
-    public static final int CONNECTION_ERROR = 1;
-    public static final int SUCESS = 2;
-    
-    protected Integer doInBackground(String... params) {
-            return sendERRMSG(params[0], params[1]);
-    }
-    
-    protected void onPostExecute(Integer result) {
-            switch(result) {
-            case SUCESS:
-                    Toast.makeText(MainActivity.this, "Sent", Toast.LENGTH_SHORT).show();
-                    break;
-            case CONNECTION_ERROR:
-                    Toast.makeText(MainActivity.this, "An error occured", Toast.LENGTH_SHORT).show();
-                    break;
-            default:
-                    break;
-            }
-    }
-    
-    int sendERRMSG(String ip, String message) {
-            try {
-                    Socket socket = new Socket(ip, printerPort);
-                    OutputStream out = socket.getOutputStream();
-                    out.write(MessageFormat.format(formatERRMSG, message).getBytes());
-                    out.close();
-                    socket.close();
-            } catch (UnknownHostException e) {
-                    e.printStackTrace();
-                    return CONNECTION_ERROR;
-            } catch (IOException e) {
-                    e.printStackTrace();
-                    return CONNECTION_ERROR;
-            }
-            return SUCESS;
-    }
-}
-
-private class sendOPMSGTask extends AsyncTask<String, Void, Integer> {
-        public static final int TOO_LONG_ERROR = 0;
-        public static final int CONNECTION_ERROR = 1;
-        public static final int SUCESS = 2;
-        
-        protected Integer doInBackground(String... params) {
-                return sendOPMSG(params[0], params[1]);
-        }
-        
-        protected void onPostExecute(Integer result) {
-                switch(result) {
-                case SUCESS:
-                        Toast.makeText(MainActivity.this, "Sent", Toast.LENGTH_SHORT).show();
-                        break;
-                case CONNECTION_ERROR:
-                        Toast.makeText(MainActivity.this, "An error occurred", Toast.LENGTH_SHORT).show();
-                        break;
-                default:
-                        break;
-                }
-        }
-        
-        int sendOPMSG(String ip, String message) {
-        try {
-                Socket socket = new Socket(ip, printerPort);
-                OutputStream out = socket.getOutputStream();
-                out.write(MessageFormat.format(formatOPMSG, message).getBytes());
-                out.close();
-                socket.close();
-        } catch (UnknownHostException e) {
-                e.printStackTrace();
-                return CONNECTION_ERROR;
-        } catch (IOException e) {
-                e.printStackTrace();
-                return CONNECTION_ERROR;
-        }
-        return SUCESS;
-}
-}
 
 }
