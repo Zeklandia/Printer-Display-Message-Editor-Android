9da8c816b2ca6ffb2d7a40e6b941ddfbc01286f9
Asher Wood <awood5659@gmail.com>
2014-01-27 18:26:46 -0800

    Clean up strings, main layout.

--- gen/zeklandia/android/printerDisplayMessageEditor/R.java
+++ gen/zeklandia/android/printerDisplayMessageEditor/R.java
@@ -51,16 +51,18 @@ public final class R {
     }
     public static final class string {
         public static final int about_title=0x7f060002;
-        public static final int action_settings=0x7f060007;
-        public static final int app_about_text=0x7f060006;
-        public static final int app_help_text=0x7f060005;
+        public static final int action_settings=0x7f060003;
         public static final int app_name=0x7f060000;
+        public static final int display_message_hint=0x7f060005;
+        public static final int error_message=0x7f060008;
         public static final int help_title=0x7f060001;
-        public static final int main_title=0x7f060008;
-        public static final int opmsg_help_text=0x7f060004;
-        public static final int pref_header_general=0x7f06000a;
-        public static final int rdymsg_help_text=0x7f060003;
-        public static final int title_activity_settings=0x7f060009;
+        public static final int main_title=0x7f060004;
+        public static final int operator_message=0x7f060009;
+        public static final int pref_header_general=0x7f06000c;
+        public static final int printer_ip_address_hint=0x7f060006;
+        public static final int ready_message=0x7f060007;
+        public static final int send_message=0x7f06000a;
+        public static final int title_activity_settings=0x7f06000b;
     }
     public static final class style {
         public static final int AppTheme=0x7f070000;
